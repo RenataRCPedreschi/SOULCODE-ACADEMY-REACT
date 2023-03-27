132a8a2e0c56a6c7371a03063288eb21e63f4289
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-03-26 22:06:28 -0300

    atualizações

--- 4 - Bibliotech React/bibliotech/src/App.jsx
+++ 4 - Bibliotech React/bibliotech/src/App.jsx
@@ -1,3 +1,17 @@
-export function App(){
-    return<></>
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Root } from "./pages/Root/Root";
+
+export function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
