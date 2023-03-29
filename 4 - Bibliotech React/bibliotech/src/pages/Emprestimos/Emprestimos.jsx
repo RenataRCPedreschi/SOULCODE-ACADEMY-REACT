a2a169595dac8c2d9c9d1979e30ef3b1f9d479f8
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-03-29 11:59:07 -0300

    atualizações tarde

---
+++ 4 - Bibliotech React/bibliotech/src/pages/Emprestimos/Emprestimos.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { Badge, Button, Container, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Loader } from "../../components/Menu/Loader/Loader";
+import { getEmprestimos } from "../../firebase/emprestimos";
+
+export function Emprestimos() {
+  /*     const data = emprestimo.dataEmprestimo.toDate().toLocaleDateString('pt-br') */
+
+  const [emprestimos, setEmprestimos] = useState(null);
+
+  useEffect(() => {
+    getEmprestimos().then((busca) => {
+      setEmprestimos(busca);
+    });
+  });
+
+  return (
+    <div className="emprestimos">
+      <Container>
+        <div className="d-flex justify-content-between align-items-center">
+          <h1>Empréstimos</h1>
+          <Button as={Link} to="/emprestimos/adicionar" variant="success">
+            Adicionar empréstimo
+          </Button>
+        </div>
+        <hr />
+
+        {emprestimos === null ? (
+          <Loader />
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Leitor</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Livro</th>
+                <th>Status</th>
+                <th>Data de Empréstimo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {emprestimos.map((emprestimo) => {
+                const dataEmprestimo = emprestimo.dataEmprestimo
+                  .toDate()
+                  .toLocaleDateString("pt-br");
+                return (
+                  <tr>
+                    <td>{emprestimo.leitor}</td>
+                    <td>{emprestimo.email}</td>
+                    <td>{emprestimo.telefone}</td>
+                    <td>{emprestimo.livro.titulo}</td>
+                    <td>
+                      <Badge
+                        bg={
+                          emprestimo.status === "Pendente"
+                            ? "warning"
+                            : "success"
+                        }
+                      >
+                        {emprestimo.status}
+                      </Badge>
+                    </td>
+                    <td>{dataEmprestimo}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        )}
+      </Container>
+    </div>
+  );
+}
